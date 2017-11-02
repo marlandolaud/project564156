84b40607f3688ac98340e674cc1a6ed182f0484a
mlaud <mlaud@outlook.com>
2017-11-02 18:05:12 -0400

    Add project files.

---
+++ Shipbob/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Shipbob.Startup))]
+namespace Shipbob
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
